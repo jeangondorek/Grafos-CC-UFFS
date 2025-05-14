1492f418afd63ba39752aadbd8d8b8421f85a114
jeangondorek <jeangondorek73@gmail.com>
2025-05-14 23:20:56 +0000

    ajustado

--- Trabalho/main.cpp
+++ Trabalho/main.cpp
@@ -20,120 +20,37 @@ using namespace std;
 
 int main()  {
     try {
-        int expression = -1;
         int vertices = 0;
+        int arestas = 0;
+        string ataca = "A";
         int v1, v2;
+
+        cin >> vertices >> arestas;
 
         Grafo grafo(vertices);
 
-        while (expression != 0) {
+        for (int i = 0; i < arestas; i++) {
+            cin >> v1 >> v2 >> ataca;
+            if (ataca == "A") {
+                Aresta aresta = Aresta(v1, v2, true);
+                grafo.AdicionaAresta(aresta);
+            }
+        }
 
-            cout << endl;
-            cout << "Escolha o que fazer: " << endl;
-            cout << "1 - Criar grafo" << endl;
-            cout << "2 - Adicionar aresta" << endl;
-            cout << "3 - Remover aresta" << endl;
-            cout << "4 - Verificar se existe aresta" << endl;
-            //cout << "5 - Verificar numero de vertices" << endl;
-            cout << "6 - Verificar numero de arestas" << endl;
-            //cout << "7 - Imprimir grafo" << endl;
-            //cout << "8 - Destruir grafo" << endl;
-            //cout << "9 - Verficia passeio" << endl;
-            //cout << "10 - Verifica caminho" << endl;
-            cout << "11 - Carrega grafo do arquivo (arquivo: grafo.txt)" << endl;
-            cout << "12 - Executa eh_bipartido_1 e eh_bipartido_2" << endl;
-            cout << "0 - Sair" << endl;
-            cin >> expression; 
-        
-            switch (expression) {
-                case 1: {
-                    grafo.DestroyGraph();
-                    cout << "Crie o grafo digitando o número de vértices: " << endl;
-                    cin >> vertices; 
-                    grafo = Grafo(vertices); 
-                    break;
-                }
-                case 2: {
-                    cout << "Digite os vertices da aresta: ";
-                    cin >> v1 >> v2;
-                    grafo.AdicionaAresta(Aresta(v1, v2, true));
-                    break;
-                }
-                case 3: {
-                    cout << "Digite os vertices da aresta: ";
-                    cin >> v1 >> v2;
-                    grafo.RemoveAresta(Aresta(v1, v2, true));
-                    break;
-                }
-                case 4: {
-                    cout << "Digite os vertices da aresta: ";
-                    cin >> v1 >> v2;
-                    Aresta aresta(v1, v2, true);
-                    cout << "Existe aresta entre " << aresta.v1 << " e " << aresta.v2 << "? " << (grafo.ExisteAresta(aresta) ? "true" : "false") << endl;
-                    break;
-                }
-                case 5:  {
-                    cout << "Grafo criado com " << grafo.NumeroVertices() << " vertices." << endl;
-                    break;
-                }
-                case 6: {
-                    cout << "Grafo criado com " << grafo.NumeroArestas() << " arestas." << endl;
-                    break;
-                }
-                case 7: {
-                    cout << endl;
-                    grafo.PrintGraph();
-                    break;
-                }
-                case 8: {
-                    grafo.DestroyGraph();
-                    break;
-                }
-                case 9: {
-                    grafo.ExistePasseio();
-                    break;
-                }
-                case 10: {
-                    std::vector<int> marcado_(grafo.NumeroVertices(), 0);
-                    int v1, v2;
-                    int identacao = 0;
-                    cout << "Digite 2 elementos para validar caminho" << endl;
-                    cin >> v1 >> v2;
-                    bool existeCamino = grafo.ExisteCaminho(v1, v2, marcado_, grafo.NumeroVertices(), identacao);
-                    if (existeCamino) {
-                        cout << "Caminho existe entre " << v1 << " e " << v2 << endl;
-                    } else {
-                        cout << "Caminho não existe entre " << v1 << " e " << v2 << endl;
-                    }
-                    break;
-                }   
-                case 11: {              
-                    grafo.CarregarGrafoDoArquivo("grafo.txt");
-                    break;
-                }   
-                case 12: {   
-                    std::vector<int> divisao1, divisao2, divisao21, divisao22;
-                    std::vector<bool> removidos(grafo.NumeroVertices(), false);
-                    std::vector<bool> removidos2(grafo.NumeroVertices(), false);
+        std::vector<int> divisao1, divisao2, divisao21, divisao22;
+        std::vector<bool> removidos(grafo.NumeroVertices(), false);
+        std::vector<bool> removidos2(grafo.NumeroVertices(), false);
 
-                    if (grafo.Bipartido1(divisao1, divisao2, removidos, 0)) {
-                        cout << "\nSIM" << endl;
-                    } else {
-                        cout << "\nNAO" << endl;
-                    }
+        if (grafo.Bipartido1(divisao1, divisao2, removidos, 0)) {
+            cout << "\nSIM" << endl;
+        } else {
+            cout << "NAO" << endl;
+        }
 
-                    if (grafo.Bipartido2(divisao21, divisao22, removidos2)) {
-                        cout << "SIM" << endl;
-                    } else {
-                        cout << "NAO" << endl;
-                    }
-                    break;
-                }
-                case 0:
-                    break;
-                default:
-                    break;
-                }
+        if (grafo.Bipartido2(divisao21, divisao22, removidos2)) {
+            cout << "SIM" << endl;
+        } else {
+            cout << "NAO" << endl;
         }
     
     } catch (const exception &e)  {
